0bf5e3243e1726cb2bb1f8d01d5ecdbac9bf50db
Eric des Courtis <eric.des.courtis@gmail.com>
2025-05-12 22:21:54 +0400

    fix: init-mongo.sh replica set

--- init-mongo.sh
+++ init-mongo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Wait for MongoDB to be available
+# Wait for MongoDB to be ready
 until mongosh --host localhost --eval "print(\"waited for connection\")"; do
   sleep 2
   echo "Waiting for MongoDB to start..."
 done
 
-# Initiate the replica set
-mongosh --host localhost < /init-mongo.js 
+# Initiate the replica set directly
+mongosh --host localhost --eval 'rs.initiate({_id: "rs0", members: [{_id: 0, host: "localhost:27017"}]})' 
