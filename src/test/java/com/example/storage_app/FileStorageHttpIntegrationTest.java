de7261d287cb4f931ecad941b2f51d8252bff726
Eric des Courtis <eric.des.courtis@gmail.com>
2025-05-12 16:18:47 +0400

    chore: Fix test for CI

--- src/test/java/com/example/storage_app/FileStorageHttpIntegrationTest.java
+++ src/test/java/com/example/storage_app/FileStorageHttpIntegrationTest.java
@@ -22,9 +22,22 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 public class FileStorageHttpIntegrationTest {
+
+  @Container static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:8.0");
+
+  @DynamicPropertySource
+  static void setProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+  }
 
   @LocalServerPort int port;
 
